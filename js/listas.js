b7f8d62595860f2780f98ec9a3faa5d1956099c3
agus2023 <landeira2017@gmail.com>
2026-06-11 21:03:52 -0300

    implemente javascript y hice que el listado de los juegos, packs y comentarios se haga de forma dinamica

---
+++ js/listas.js
@@ -0,0 +1,79 @@
+export let lista_juegos = [
+            {
+                titulo:"THE LAST OF US",srcImagen:"/img/The_last_of_us.jpg",
+                descripcion:"Es un juego de aventura y supervivencia con una historia muy profunda y emotiva. ",
+                genero:"Accion, Aventura, Supervivencia y Drama.",precio:60000},
+                
+            {   
+                titulo:"GTA V",srcImagen:"img/Gta.jpg", 
+                genero:"Accion, Aventura y Mundo abierto.",precio:80000,
+                descripcion:"Es un juego de mundo abierto donde podés explorar una ciudad enorme, realizar misiones y hacer prácticamente lo que quieras.",
+            },
+
+            {
+                titulo:"Rocket League",srcImagen:"img/Rocket_league.jpg",
+                descripcion:"Es un juego que combina fútbol con autos, donde tenés que hacer goles manejando autos. Es rápido, divertido y requiere mucha habilidad y reflejos.",
+                genero:"Deportes",precio:10000
+            },
+
+            {
+                titulo:"Call of duty",srcImagen:"img/Cod.jpg",
+                descripcion:"Es un juego de disparos en primera persona con modos de campaña y multijugador. Tiene mucha acción, armas variadas y mapas dinámicos.",
+                genero:"Accion y Shooter",precio:40000
+            }
+]
+
+export let lista_packs = [
+
+    {
+        nombre:"Resident Evil",
+        descripcion: "Aprovecha de este pack impresionante que te va a permitir jugar algunos juegos increibles que tiene esta maravillosa saga de Resident Evil a tan solo: ",
+        precio: 30000,
+        imagenes: [
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/co1taw.webp","Residen Evil 0"],
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/cobtwh.webp","Residen Evil 7: Biohazard"],
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/coab9q.webp","Resident Evil 8: Village"],
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/cobgod.webp","Resident Evil 6"],
+
+        ]
+
+    },
+    {
+        nombre:"Uncharted",
+        descripcion: "Aprovecha de este pack para jugar la aventura completa de Nathan Drake a tan solo: ",
+        precio: 40000,
+        imagenes: [
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/co1tp7.webp","Uncharted 1"],
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/co1tnb.webp","Uncharted 2"],
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/co1tp8.webp","Uncharted 3"],
+            ["https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7h.webp","Uncharted 4"],
+
+        ]
+
+    },
+]
+
+export let lista_comentarios = [
+
+    {
+        mensaje: "“Jugue al the last of us y la verdad una maravilla, gracias por recomendarlo”",
+        foto:"img/foto-de-perfil.png",
+        usuario:"Nacho Benitez"
+
+    },{
+        mensaje: "“Que bueno Mario Bross”",
+        foto:"img/foto-de-perfil.png",
+        usuario:"Santi Fernandez"
+
+    },{
+        mensaje: "“Muy buenas recomendaciones !”",
+        foto:"img/foto-de-perfil.png",
+        usuario:"Valen miño"
+
+    },{
+        mensaje: "“El pack de Resident Evil es una locura”",
+        foto:"img/foto-de-perfil.png",
+        usuario:"Thiago romero"
+
+    },
+]
